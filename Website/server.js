e28b3254f78bfa3b6700fdba77f65f21bc8bf3e5
mwbauer459 <35506514+mwbauer459@users.noreply.github.com>
2018-02-19 19:43:36 -0800

    Add files via upload

--- Website/server.js
+++ Website/server.js
@@ -5,20 +5,23 @@ var path = require('path');
 var app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('views',  path.join(__dirname, "views"));
 
-var dbcon = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    database: "mydb",
-    multipleStatements: true
+var dbcon = mysql.createConnection(
+{
+	host: "unitokendb.cpcqiko7dvv7.us-east-1.rds.amazonaws.com",
+	user: "root",
+	password: "uniaccess",
+	port: "3306",
+	database: "db"
+	
 });
 
-/* Routing of links */
-routes(app, dbcon);
+dbcon.connect(function(err)
+{
+	if (err) throw err;
+	console.log("connected!");
 
-/* Application is bound to port 3000 */
-app.listen(3000, function() {
-    console.log('Server is bound to port 3000')
+
 });
+
