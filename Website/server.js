0d3bdde535b8529a14d7162e5c3bbadb7909cd83
Max Bauer <35506514+mwbauer459@users.noreply.github.com>
2018-02-27 15:45:59 -0800

    Update server.js

--- Website/server.js
+++ Website/server.js
@@ -1,33 +1,53 @@
 var express = require('express');
 var routes = require('./index.js');
-var market = require('./marketplace.js');
 var mysql = require('mysql');
 var path = require('path');
 var app = express();
 
 app.set('view engine', 'ejs');
-app.set('views',  path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('views',  path.join(__dirname, "views"));
 
 var dbcon = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	database: 'mydb'
+	host: "unitokendata.cpcqiko7dvv7.us-east-1.rds.amazonaws.com",
+	user: "root",
+	password: "dbaccess",
+	port: "3306",
+	database: "db"
 });
 
-dbcon.connect(function(err) {
-	if (err) throw err;
+//used for search function
+app.set('views',__dirname + '/views');
+app.use(express.static(__dirname + '/JS'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.get('/',function(req,res){
+res.render('index.html');
 });
 
+app.get('/search',function(req,res){
+
+});
+app.get('/',function(req,res){
+res.render('index.html');
+});
+
+app.get('/search',function(req,res){
+connection.query('SELECT wallet_address from wallet where user_id like "%'+req.query.key+'%"',
+function(err, rows, fields) {
+if (err) throw err;
+var data=[];
+for(i=0;i<rows.length;i++)
+{
+data.push(rows[i].first_name);
+}
+res.end(JSON.stringify(data));
+});
+});
 
 /* Routing of links */
 routes(app, dbcon);
 
-/* Temp marketplace */
-market(app, dbcon);
-
-
-/* Application is bound to port 3000 */
-app.listen(3000, function(){
-	console.log('Server is bound to port 3000');
+var server=app.listen(3000,function(){
+console.log("We have started our server on port 3000");
 });
