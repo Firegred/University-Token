2ed5cf1a12d05de68145cfe0d933266ebed42c60
Ormakent <35436986+Ormakent@users.noreply.github.com>
2018-02-27 22:01:34 -0800

    Added login functionality, integrated registration with email confirmation

--- Website/index.js
+++ Website/index.js
@@ -1,16 +1,87 @@
 var nodemailer = require('nodemailer');
 var bodyParser = require('body-parser');
+var crypto = require("crypto");
+var path = require('path');
+var multer = require('multer');
+var LocalStrategy = require('passport-local').Strategy;
+var storage = multer.diskStorage({
+    destination: "uploads/",
+    filename: function(req, file, callback) {
+        crypto.pseudoRandomBytes(16, function(err, raw) {
+            if (err) return callback(err);
+            callback(null, raw.toString('hex') + path.extname(file.originalname));
+        });
+    }
+});
+var upload = multer({
+    storage: storage
+});
 
-module.exports = function(app, dbcon) {
+
+module.exports = function(app, dbcon, passport) {
 
     app.use(bodyParser.urlencoded({
         extended: true
     }));
 
+    passport.serializeUser(function(user, done) {
+        done(null, user.user_id);
+    });
+
+    passport.deserializeUser(function(id, done) {
+        dbcon.query("SELECT * FROM perm_users WHERE user_id = ?", [id], function(err, rows) {
+            done(err, rows[0]);
+        });
+    });
+
+    passport.use('local-login',
+        new LocalStrategy({
+                usernameField: 'email',
+                passwordField: 'password'
+            },
+            function(email, password, done) {
+                dbcon.query("SELECT * FROM perm_users WHERE email = " + dbcon.escape(email), function(err, rows) {
+                    if (err) {
+                        return done(err);
+                    }
+                    if (!rows.length) {
+                        return done(null, false);
+                    }
+
+                    if (rows[0].password != password) {
+                        return done(null, false);
+                    } else {
+                        return done(null, rows[0]);
+                    }
+                });
+            }));
+
+    app.post("/login", passport.authenticate('local-login', {
+            successRedirect: "/success",
+            failureRedirect: "/err"
+        }),
+        function(req, res) {
+            if (req.body.remember) {
+                req.session.cookie.maxAge = 1000 * 60 * 3;
+            } else {
+                req.session.cookie.expires = false;
+            }
+        });
+
+    function isLoggedIn(req, res, next) {
+
+        // if user is authenticated in the session, carry on
+        if (req.isAuthenticated())
+            return next();
+
+        // if they aren't redirect them to the home page
+        res.redirect('/');
+    }
+
     /*
         Description:
         TODO(-s):
-    */
+        */
     app.get('/', function(req, res) {
         res.render('index')
     })
@@ -34,45 +105,45 @@ module.exports = function(app, dbcon) {
         }
     });
 
-    var host = "localhost:3000";
+    var host = "test.universitytoken.net";
 
     /*
         Description:
         TODO(-s):
-    */
-    app.post('/sendconfirmation', function(req, res) {
+        */
+    function sendEmailConfirmation(email, res) {
         var confirmationToken = (1 + Math.random()).toString(36).substring(2, 18);
-        var databaseQuery = "UPDATE temp_users SET verification_code = '" +
-            confirmationToken + "' WHERE email = " + dbcon.escape(req.body.email);
+        var databaseQuery = "UPDATE temp_users SET verification_code = " +
+            dbcon.escape(confirmationToken) + " WHERE email = " + dbcon.escape(email);
         dbcon.query(databaseQuery, function(err, result) {
             if (err) {
                 throw err;
             }
-            console.log(result.affectedRows + " record(s) updated");
-        });
-        var link = "http://" + host + "/verify?id=" + confirmationToken;
-        var mailOptions = {
-            from: 'Do Not Reply <unitokenemailconfirmation@gmail.com>',
-            to: req.body.email,
-            subject: 'Unitoken registration confirmation',
-            html: 'Please click on the link to verify your e-mail.<br><a href=' +
+            console.log(email);
+            var link = "http://" + host + "/verify?id=" + confirmationToken;
+            var mailOptions = {
+                from: 'Do Not Reply <unitokenemailconfirmation@gmail.com>',
+                to: email,
+                subject: 'Unitoken registration confirmation',
+                html: 'Please click on the link to verify your e-mail.<br><a href=' +
                 link + '>Click here to verify</a>'
-        };
-        smtpTransport.sendMail(mailOptions, function(error, response) {
-            if (error) {
-                console.log(error);
-                res.end("error");
-            } else {
-                console.log("Message sent: " + response.message);
-                res.end("Please, check your email to verify your account.");
-            }
+            };
+            smtpTransport.sendMail(mailOptions, function(error, response) {
+                if (error) {
+                    console.log(error);
+                    res.end("error");
+                } else {
+                    console.log("Message sent: " + response.message);
+                    res.end("Please, check your email to verify your account.");
+                }
+            });
         });
-    });
+    }
 
     /*
         Description:
         TODO(-s):
-    */
+        */
     app.get('/verify', function(req, res) {
         if ((req.protocol + "://" + req.get('host')) == ("http://" + host)) {
             var databaseQuery = "SELECT * FROM temp_users WHERE verification_code = " + dbcon.escape(req.query.id);
@@ -85,7 +156,18 @@ module.exports = function(app, dbcon) {
                     res.end("This link has expired.");
                 } else {
                     databaseQuery = "DELETE FROM temp_users WHERE email = " + dbcon.escape(result[0].email) + "; ";
-                    databaseQuery += "INSERT INTO perm_users (email, password) values (" + dbcon.escape(result[0].email) + ");";
+                    databaseQuery += "INSERT INTO perm_users (first_name,last_name,email,birth_month,birth_day,birth_year,university,country,state,zip,password) VALUES (" +
+                        dbcon.escape(result[0].first_name) + "," +
+                        dbcon.escape(result[0].last_name) + "," +
+                        dbcon.escape(result[0].email) + "," +
+                        dbcon.escape(result[0].birth_month) + "," +
+                        dbcon.escape(result[0].birth_day) + "," +
+                        dbcon.escape(result[0].birth_year) + "," +
+                        dbcon.escape(result[0].university) + "," +
+                        dbcon.escape(result[0].country) + "," +
+                        dbcon.escape(result[0].state) + "," +
+                        dbcon.escape(result[0].zip) + "," +
+                        dbcon.escape(result[0].password) + ")";
                     dbcon.query(databaseQuery, function(err, result) {
                         if (err) throw err;
                         console.log(result.affectedRows + " record(s) updated");
@@ -101,7 +183,7 @@ module.exports = function(app, dbcon) {
     /*
         Description:
         TODO(-s):
-    */
+        */
     app.get('/resetpassword', function(req, res) {
         res.render('resetpassword');
     });
@@ -109,7 +191,7 @@ module.exports = function(app, dbcon) {
     /*
         Description:
         TODO(-s):
-    */
+        */
     app.post('/resetpassword', function(req, res) {
         var databaseQuery = "SELECT * FROM reset_pass WHERE email = " + dbcon.escape(req.body.email) + " AND used_flag = 0";
         dbcon.query(databaseQuery, function(err, result) {
@@ -136,7 +218,7 @@ module.exports = function(app, dbcon) {
                                 to: req.body.email,
                                 subject: 'Unitoken password reset',
                                 html: 'Please click on the link to reset your password.<br><a href=' +
-                                    link + '>Click here to reset your password</a>'
+                                link + '>Click here to reset your password</a>'
                             };
                             smtpTransport.sendMail(mailOptions, function(error, response) {
                                 if (error) {
@@ -164,7 +246,7 @@ module.exports = function(app, dbcon) {
     /*
         Description:
         TODO(-s):
-    */
+        */
     app.get("/reset", function(req, res) {
         if ((req.protocol + "://" + req.get('host')) == ("http://" + host)) {
             var databaseQuery = "SELECT * FROM reset_pass WHERE verification_code =" + dbcon.escape(req.query.id) + " AND used_flag = 0;";
@@ -186,7 +268,7 @@ module.exports = function(app, dbcon) {
     /*
         Description:
         TODO(-s):
-    */
+        */
     app.post("/reset", function(req, res) {
         var databaseQuery = "SELECT * FROM reset_pass WHERE verification_code =" + dbcon.escape(req.query.id) + " AND used_flag = 0;";
         dbcon.query(databaseQuery, function(err, result) {
@@ -202,8 +284,62 @@ module.exports = function(app, dbcon) {
         });
     });
 
-    app.get("/regtest", function(req, res) {
-        res.render("registrationpage");
+    app.get("/register", function(req, res) {
+        var email = req.query.email;
+        res.render("registrationpage", {
+            email: email
+        });
     });
+
+    app.post('/register', isAlreadyRegistered, function(req, res) {
+        var databaseQuery = "INSERT INTO temp_users (first_name,last_name,email,birth_month,birth_day,birth_year,university,country,state,zip,password) VALUES (" +
+            dbcon.escape(req.body.firstName) + "," +
+            dbcon.escape(req.body.lastName) + "," +
+            dbcon.escape(req.body.email) + "," +
+            dbcon.escape(req.body.birthmonth) + "," +
+            dbcon.escape(req.body.birthday) + "," +
+            dbcon.escape(req.body.birthyear) + "," +
+            dbcon.escape(req.body.university) + "," +
+            dbcon.escape(req.body.country) + "," +
+            dbcon.escape(req.body.state) + "," +
+            dbcon.escape(req.body.zip) + "," +
+            dbcon.escape(req.body.password) + ")";
+
+        dbcon.query(databaseQuery, function(err, result) {
+            if (err) {
+                throw err;
+            } else {
+                console.log(result.affectedRows + "records updated.");
+                sendEmailConfirmation(req.body.email, res);
+            }
+        });
+    });
+
+    function isAlreadyRegistered (req, res, next) {
+        var databaseQuery = "SELECT * FROM perm_users where email = " + dbcon.escape(req.body.email);
+        dbcon.query(databaseQuery, function(err, result){
+           if(!result.length){
+               return next();
+           } else {
+               res.end("This e-mail is already in use.");
+           }
+        });
+    }
+
+    app.get("/market/post", function(req, res) {
+        res.render("createListing");
+    });
+
+    app.post("/market/post", upload.single("uploadPhoto"), function(req, res) {
+        var databaseQuery = "INSERT INTO listings (user_id, name, price, category, bio, info, picture) VALUES (" +
+            "0" + ", " + dbcon.escape(req.body.listingName) + ", " + dbcon.escape(req.body.listingPrice) +
+            ", " + dbcon.escape(req.body.listingCategory) + ", " + dbcon.escape(req.body.listingBio) +
+            ", " + dbcon.escape(req.body.listingInfo) + ", " + dbcon.escape(req.file.path) + ");";
+        dbcon.query(databaseQuery, function(err, result) {
+            res.end("Listing was created successfully");
+        });
+    });
+
+
 
 };
--- Website/server.js
+++ Website/server.js
@@ -2,52 +2,30 @@ var express = require('express');
 var routes = require('./index.js');
 var mysql = require('mysql');
 var path = require('path');
-var app = express();
+var session = require('express-session');
+var passport = require('passport');
+var cookieParser = require('cookie-parser');
 
+var app = express();
 app.set('view engine', 'ejs');
-app.set('views',  path.join(__dirname, "views"));
+app.set('views', path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+app.use(session({secret: ""})); // Please insert random string for secret
+app.use(passport.initialize());
+app.use(passport.session());
 
 var dbcon = mysql.createConnection({
-	host: "unitokendata.cpcqiko7dvv7.us-east-1.rds.amazonaws.com",
-	user: "root",
-	password: "dbaccess",
-	port: "3306",
-	database: "db"
-});
-
-//used for search function
-app.set('views',__dirname + '/views');
-app.use(express.static(__dirname + '/JS'));
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
-
-app.get('/',function(req,res){
-res.render('index.html');
-});
-
-app.get('/search',function(req,res){
-
-});
-app.get('/',function(req,res){
-res.render('index.html');
-});
-
-app.get('/search',function(req,res){
-connection.query('SELECT wallet_address from wallet where user_id like "%'+req.query.key+'%"',
-function(err, rows, fields) {
-if (err) throw err;
-var data=[];
-for(i=0;i<rows.length;i++)
-{
-data.push(rows[i].first_name);
-}
-res.end(JSON.stringify(data));
-});
+    host: "localhost",
+    user: "root",
+    database: "mydb",
+    multipleStatements: true
 });
 
 /* Routing of links */
-routes(app, dbcon);
+routes(app, dbcon, passport);
 
-var server=app.listen(3000,function(){
-console.log("We have started our server on port 3000");
+/* Application is bound to port 3000 */
+app.listen(3000, function() {
+    console.log('Server is bound to port 3000')
 });
