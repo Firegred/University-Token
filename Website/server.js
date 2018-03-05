ae0ee6f99b1df838c6e888901f19cca01733b6b8
Nicolas Shak <nshak@ucsc.edu>
2018-03-04 19:36:25 -0800

    fixed server.js not serving from public folder.

--- Website/server.js
+++ Website/server.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var routes = require('./index.js');
+var marketplace = require('./marketplace.js');
 var mysql = require('mysql');
 var path = require('path');
 var app = express();
 
 app.set('view engine', 'ejs');
 app.set('views',  path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 var dbcon = mysql.createConnection({
 	host: "unitokendata.cpcqiko7dvv7.us-east-1.rds.amazonaws.com",
@@ -15,38 +18,34 @@ var dbcon = mysql.createConnection({
 	database: "db"
 });
 
+//For local testing
+/*
+var dbcon = mysql.createConnection({ 
+	host: 'localhost',
+	user: 'root',
+	database: 'mydb'
+});
+*/
 //used for search function
 app.set('views',__dirname + '/views');
 app.use(express.static(__dirname + '/JS'));
-app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-app.get('/',function(req,res){
-res.render('index.html');
-});
-
 app.get('/search',function(req,res){
-
-});
-app.get('/',function(req,res){
-res.render('index.html');
-});
-
-app.get('/search',function(req,res){
-connection.query('SELECT wallet_address from wallet where user_id like "%'+req.query.key+'%"',
-function(err, rows, fields) {
-if (err) throw err;
-var data=[];
-for(i=0;i<rows.length;i++)
-{
-data.push(rows[i].first_name);
-}
-res.end(JSON.stringify(data));
-});
+	connection.query('SELECT wallet_address from wallet where user_id like "%'+req.query.key+'%"',
+	function(err, rows, fields) {
+		if (err) throw err;
+			var data=[];
+		for(i=0;i<rows.length;i++) {
+			data.push(rows[i].first_name);
+		}
+		res.end(JSON.stringify(data));
+	});
 });
 
 /* Routing of links */
 routes(app, dbcon);
+marketplace(app, dbcon);
 
 var server=app.listen(3000,function(){
 console.log("We have started our server on port 3000");
