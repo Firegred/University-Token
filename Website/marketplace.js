160b58a3c914d0f4ae800813fbc5b03026da39b5
Alexander Kiryukhin <alexanderkiryukhin@MacBook-Pro-Alexander.local>
2018-03-09 17:04:19 -0800

    added completed listings, marketplace bug fixes

--- Website/marketplace.js
+++ Website/marketplace.js
@@ -3,8 +3,8 @@ const path = require('path');
 const multer = require('multer');
 const storage = multer.diskStorage({
     destination: "uploads/",
-    filename: function(req, file, callback) {
-        crypto.pseudoRandomBytes(16, function(err, raw) {
+    filename: function (req, file, callback) {
+        crypto.pseudoRandomBytes(16, function (err, raw) {
             if (err) return callback(err);
             callback(null, raw.toString('hex') + path.extname(file.originalname));
         });
@@ -16,39 +16,39 @@ const upload = multer({
 
 module.exports = function (app, dbcon) {
 
-     app.get('/market', function(req, res) {
-        dbcon.query("SELECT * FROM listings", function(err, result, fields) {
+    app.get('/market', function (req, res) {
+        dbcon.query("SELECT * FROM listings", function (err, result, fields) {
             if (err) throw err;
-            console.log(result);
-            res.render('marketplace', { listings: result });
+            res.render('marketplace', {listings: result, user: req.user});
         });
     })
 
-    app.get("/market/post", function(req, res) {
-        if(!req.isAuthenticated()){
+    app.get("/market/post", function (req, res) {
+        if (!req.isAuthenticated()) {
             res.redirect("/login");
+        } else {
+            res.render("createListing", {user: req.user});
         }
-        res.render("createListing",{user: req.user});
     });
 
-    app.post("/market/post", upload.single("uploadPhoto"), function(req, res) {
+    app.post("/market/post", upload.single("uploadPhoto"), function (req, res) {
         var picture = (typeof req.file == "undefined") ? "" : req.file.path;
-        var databaseQuery = "INSERT INTO listings (user_id, name, price, category, bio, info, picture) VALUES (" +
-            "0" + ", " + dbcon.escape(req.body.listingName) + ", " + dbcon.escape(req.body.listingPrice) +
-            ", " + dbcon.escape(req.body.listingCategory) + ", " + dbcon.escape(req.body.listingBio) +
-            ", " + dbcon.escape(req.body.listingInfo) + ", " + dbcon.escape(picture) + ");";
-        dbcon.query(databaseQuery, function(err, result) {
-            res.end("Listing was created successfully");
-        });
+        var databaseQuery = "INSERT INTO listings (user_id, name, price, category, bio, info, picture) VALUES " +
+            "(?, ?, ?, ?, ?, ?, ?)";
+        dbcon.query(databaseQuery, [req.user.user_id, req.body.listingName, req.body.listingPrice,
+                req.body.listingCategory, req.body.listingBio, req.body.listingInfo, picture],
+            function (err, result) {
+                res.end("Listing was created successfully");
+            });
     });
 
-    app.get("/market/view/:id", function(req, res) {
+    app.get("/market/view/:id", function (req, res) {
         var listingId = req.params.id;
         dbcon.query("SELECT * FROM listings WHERE id = ?", [listingId], function (err, rows) {
             if (err) {
                 throw err;
             }
-            if(!rows.length){
+            if (!rows.length) {
                 res.end("No listing with such id");
             } else {
                 res.render("viewListing", {listing: rows[0]});
@@ -56,38 +56,62 @@ module.exports = function (app, dbcon) {
         });
     });
 
-    app.get("/market/buy", function(req, res){
-        if(!req.isAuthenticated()){
+    app.get("/market/buy", function (req, res) {
+        if (!req.isAuthenticated()) {
             res.redirect("/login");
-        }
-        var listingId = req.query.id;
-        dbcon.query("SELECT * FROM listings where id = ?", [listingId], function(err, result){
-            if(!result.length){
-                res.end("No such listing");
-            } else {
-                if(hasEnoughFunds()){
-                    dbcon.query("DELETE FROM listings WHERE id = ?", [listingId], function(err, rows){
-                        if(err){
-                            throw err;
+        } else {
+            var listingId = req.query.id;
+            dbcon.query("SELECT * FROM listings where id = ?", [listingId], function (err, result) {
+                if (!result.length) {
+                    res.end("No such listing");
+                } else {
+                    if(req.user.user_id != result[0].user_id) {
+                        if (hasEnoughFunds()) {
+                            if (performTransaction()) {
+                                dbcon.query("DELETE FROM listings WHERE id = ?", [listingId], function (err, rows) {
+                                    if (err) {
+                                        throw err;
+                                    }
+                                    addListingToCompleted(result[0], dbcon, req.user.user_id);
+                                    notifySeller();
+                                    res.end("Transaction was successful");
+                                });
+                            } else {
+                                res.end("There was an error with your transaction. Please, retry later.")
+                            }
                         }
-                        addListingToCompleted(rows[0]);
-                        performTransaction();
-                        notifySeller();
-                        res.end("Transaction was successful");
-                    });
+                    } else {
+                        res.end("You can't buy your own listings");
+                    }
                 }
-            }
-        })
+            });
+        }
 
     });
 
-    function hasEnoughFunds(){
+    function hasEnoughFunds() {
         return true;
     }
 
-    function performTransaction(){}
+    function performTransaction() {
+        return true;
+    }
 
-    function addListingToCompleted(listing){}
+    function addListingToCompleted(listing, dbcon, buyerId) {
+        var dbQuery = "INSERT INTO completed_listings (id, buyer_id, seller_id, name, price, category, bio, info, picture)" +
+            " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        dbcon.query(dbQuery, [listing.id, buyerId, listing.user_id, listing.name, listing.price,
+                listing.category, listing.bio, listing.info, listing.picture],
+            function (err, rows) {
+                if (err) {
+                    throw err;
+                } else {
+                    console.log("Listing with id " + listing.id + " was successfully moved to completed listings");
+                }
+            });
+    }
 
-    function notifySeller(){}
+    function notifySeller() {
+
+    }
 }
