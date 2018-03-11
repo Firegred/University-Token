acda21b67ff0475870a1c19659b3fba76a7aab7b
Andrew Boyadjiev <bcowbob2@gmail.com>
2018-03-10 22:24:48 -0800

    Updated for web3 support

--- Website/server.js
+++ Website/server.js
@@ -1,29 +1,28 @@
-var express = require('express');
-var routes = require('./index.js');
-var mysql = require('mysql');
-var path = require('path');
-var session = require('express-session');
-var passport = require('passport');
-var cookieParser = require('cookie-parser');
+const express = require('express');
+const mysql = require('mysql');
+const path = require('path');
+const session = require('express-session');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const web3 = require('web3');
+const fs = require('fs');
 
 var app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
-app.use(session({secret: "ahsbd1928HAIBDS819237123"}));
+app.use(session({secret: "1"})); // Please insert random string for secret
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(bodyParser.urlencoded({extended: true}));
 
-var dbcon = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "security",
-    database: "mydb",
-    multipleStatements: true
-});
+const dbConfig = JSON.parse(fs.readFileSync("config/database.json"));
+var dbcon = mysql.createConnection(dbConfig);
 
 /* Routing of links */
+const routes = require('./index.js');
 routes(app, dbcon, passport);
 
 /* Application is bound to port 3000 */
